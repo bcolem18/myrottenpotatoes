85fb57b6e3ebacd7b0dfed87623f3b4cf085ef54
saasbook <saasbook@saasbook.(none)>
2014-10-13 14:42:30 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = '3c6ddd5be1082e312529823fbe4a6893677c16accdc7d619fe9f47d3ab52bc6964d424b571743bf715128a33ae70d146f276699b337fae14aef3982be265ddfc'
